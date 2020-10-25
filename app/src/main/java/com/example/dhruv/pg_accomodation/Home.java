6888120ccc6326221df37e10775fca34b0dbd6ea
Shivang29185 <shivangbgohel@gmail.com>
2020-10-25 11:43:11 +0530

    added profile and home fragment

--- app/src/main/java/com/example/dhruv/pg_accomodation/Home.java
+++ app/src/main/java/com/example/dhruv/pg_accomodation/Home.java
@@ -1,12 +1,16 @@
 package com.example.dhruv.pg_accomodation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Home extends AppCompatActivity {
@@ -27,6 +31,35 @@ public class Home extends AppCompatActivity {
                 startActivity(new Intent(Home.this,LoginPage.class));
             }
         });
+
+        BottomNavigationView navigationView =findViewById(R.id.bottom_navigation);
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                int id =menuItem.getItemId();
+
+                if(id == R.id.home){
+                    HomeFragment fragment =new HomeFragment();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.framelayout, fragment);
+                    fragmentTransaction.commit();
+                }
+                if(id == R.id.Upload){
+                    startActivity(new Intent(Home.this,Profile.class));
+                }
+                if(id == R.id.profile){
+                    ProfileFragment fragment =new ProfileFragment();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.framelayout, fragment);
+                    fragmentTransaction.commit();
+                }
+                return true;
+            }
+        });
+
+        navigationView.setSelectedItemId(R.id.home);
 
     }
 }
